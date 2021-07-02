afd52c704f910538471ee6503dd691c45ab521c5
Galuh Pradipta <galuh.pradipta@qoala.id>
2021-07-02 12:39:07 +0700

    adding middleware logger and recover for htp

--- app/main.go
+++ app/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/galuhpradipta/go-auth-service/models"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 
 	userHandler "github.com/galuhpradipta/go-auth-service/domain/user/delivery/http"
 	userRepository "github.com/galuhpradipta/go-auth-service/domain/user/repository"
@@ -16,14 +17,14 @@ import (
 )
 
 func main() {
-	fmt.Println("go auth service")
 	fiber := fiber.New()
+	fiber.Use(recover.New())
+	fiber.Use(logger.New())
 
 	db, err := gorm.Open(sqlite.Open("user.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	db.AutoMigrate(&models.User{})
 
 	userRepository := userRepository.NewRepository(db)
