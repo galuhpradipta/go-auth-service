7498d6bf53279af107d5200786441adb81c2aafb
Galuh Pradipta <galuh.pradipta@qoala.id>
2021-07-02 14:17:28 +0700

    adding extract token

--- shared/jwt/jwt.go
+++ shared/jwt/jwt.go
@@ -1,7 +1,10 @@
 package jwt
 
 import (
+	"strings"
 	"time"
+
+	"github.com/gofiber/fiber/v2"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/galuhpradipta/go-auth-service/shared/constant"
@@ -21,4 +24,39 @@ func GenerateSessionToken(email string) (string, error) {
 
 	}
 	return tokenString, nil
+}
+
+func ExtractToken(c *fiber.Ctx) (string, error) {
+	token, err := verifyToken(c)
+	if err != nil {
+		return "", err
+	}
+
+	return token.Claims.(jwt.MapClaims)["email"].(string), err
+}
+
+func extractAuthHeader(c *fiber.Ctx) string {
+	bearerToken := c.Get("Authorization")
+
+	// Normally Authorization HTTP header.
+	onlyToken := strings.Split(bearerToken, " ")
+	if len(onlyToken) == 2 {
+		return onlyToken[1]
+	}
+
+	return ""
+}
+
+func verifyToken(c *fiber.Ctx) (*jwt.Token, error) {
+	tokenString := extractAuthHeader(c)
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(constant.JWT_SECRET_KEY), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
 }
