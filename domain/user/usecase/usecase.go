c09cc6a9cb5dc9fbe196aefceaf4a9e41cb32d53
Galuh Pradipta <galuh.pradipta@qoala.id>
2021-07-02 12:05:02 +0700

    adding usecase

--- domain/user/usecase/usecase.go
+++ domain/user/usecase/usecase.go
@@ -1,1 +1,13 @@
 package usecase
+
+import "github.com/galuhpradipta/go-auth-service/domain/user"
+
+type usecase struct {
+	userRepository user.Repository
+}
+
+func NewUsecase(userRepository user.Repository) user.Usecase {
+	return &usecase{
+		userRepository: userRepository,
+	}
+}
